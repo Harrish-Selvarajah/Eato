2f740fbade4877d83e747444c7e495c9a729b3e5
Amjad <amjadnzr@outlook.com>
2021-01-06 13:07:44 +0530

    Added search and sellect all features

--- js/favourites.js
+++ js/favourites.js
@@ -1,5 +1,7 @@
 // Global Variables
+var allFavVendorList = []
 var favVendorList = []
+var selectedFavList = []
 var demoUserId = "786"
 
 var firebaseConfig = {
@@ -24,8 +26,9 @@ var firebaseConfig = {
 
     if (Object.keys(data).includes(demoUserId)){
             if(data[demoUserId]['favourites'] != undefined || data[demoUserId]['favourites'].length != 0){
-                console.info("STARTING TO LOAD FAVOURITES LIST")
+                console.log("STARTING TO LOAD FAVOURITES LIST")
                 favVendorList = data[demoUserId]['favourites'];
+                allFavVendorList = favVendorList;
                 loadFavVendorList();
             }else{
                 console.log("USER HAS NOT FAVOURITED ANYTHING")
@@ -41,11 +44,94 @@ function (error) {
 
 
 function loadFavVendorList() {
-    
-    $selectAllContainer = $('.select-all-container')
-        console.log($selectAllContainer)
-        console.log(favVendorList)
+
+    $flexContainer = $('.flex-container')
+    $(".flex-container .poi-container").remove();
+
+    var poiContent = ""
+
     favVendorList.forEach(favVendor => {
+        poiContent = poiContent.concat(`<!-- Start of POI -->
+        <div class="poi-container" id="vendor-${favVendor.vendorID}">
+            <div class="image-container">
+                <div class="heart-container"> 
+                    <i class=" material-icons icon-fav">favorite</i>
+                </div>
+            </div>
+
+            <div class="info-container">
+                <div class= "info-line-1">
+                    <h2>${favVendor.vendorName}</h2>
+                    <input type="checkbox" id="" class="checkbox"/>
+                </div>
+                <div class= "info-line-2">
+                    <i class=" material-icons icon-star">grade</i>
+                    <h4>${favVendor.Rating}</h4>
+                </div>
+            </div>    
+        </div>
+        <!-- End of POI -->`)       
+    });
+    
+   $flexContainer.append(poiContent)
+}
+
+
+$(document).ready(function(){
+
+    // Close PopUp doesnt work 
+    $('#close-click').click(function(){
+        console.log("Close")
+         $("#popupLogin-popup").hide()
+    })
+
+    // Open -But doesnt work 
+    $(".share-button-container").click(function () { 
+        console.log("Open")
+        $("#popupLogin-popup").show();
         
     });
-}
+     
+   $("#select-all").click(function () { 
+       
+    if ($('#select-all').is(":checked")) {
+        
+        console.log("Click")
+        $('.poi-container').find(':checkbox').each(function(element){
+            $(this).prop('checked', true);
+         });
+
+    // Add all favourites in the screen to selected favourites
+    selectedFavList.concat(favVendorList);
+
+    }else{
+
+        console.log("Un-click")
+
+        $('.poi-container').find(':checkbox').each(function(element){
+            $(this).prop('checked', false);
+         });
+
+    // Unselected all 
+      selectedFavList = favVendorList.filter(function(element){
+          return !selectedFavList.includes(element)
+      })
+    } 
+
+    console.log(selectedFavList)
+   });
+
+
+   //Searching Feature 
+   $("#search-bar").change(function (e) { 
+      console.log(e.target.value)
+
+      favVendorList = allFavVendorList.filter(function (element) {  
+          console.log(element.vendorName)
+        return element.vendorName.toUpperCase().includes(e.target.value.toUpperCase());
+      })
+      console.log(favVendorList)
+      loadFavVendorList();
+       
+   });
+  });
