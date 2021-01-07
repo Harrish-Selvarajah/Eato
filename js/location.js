0b65dc5ac5de8efe6deb0b9767a2bea765791f46
Harrish Selvarajah <harrish@codemasker.lk>
2021-01-07 13:00:33 +0530

    chnages

--- js/location.js
+++ js/location.js
@@ -75,10 +75,12 @@ function initAutocomplete() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
+          console.log(position, "position");
           const pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
+          codeLatLng( position.coords.latitude, position.coords.longitude);
           infoWindow.setPosition(pos);
           infoWindow.setContent("Location found.");
           infoWindow.open(map);
@@ -146,5 +148,40 @@ function saveLocation() {
   if (userLocation !== "") {
     savedLocation.push(userLocation);
   }
+}
+
+function codeLatLng(lat, lng) {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(lat, lng);
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+    console.log(results)
+      if (results[1]) {
+       //formatted address
+      //  alert(results[0].formatted_address)
+      console.log(results[0].formatted_address);
+      //find country name
+           for (var i=0; i<results[0].address_components.length; i++) {
+          for (var b=0;b<results[0].address_components[i].types.length;b++) {
+
+          //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+              if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                  //this is the object you are looking for
+                  city= results[0].address_components[i];
+                  break;
+              }
+          }
+      }
+      //city data
+      // alert(city.short_name + " " + city.long_name)
+      console.log(city.short_name + " " + city.long_name)
+
+      } else {
+        alert("No results found");
+      }
+    } else {
+      alert("Geocoder failed due to: " + status);
+    }
+  });
 }
 
