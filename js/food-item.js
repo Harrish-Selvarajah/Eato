16643452eed8860cbbbeb68f69b13cb4c6111510
Harrish Selvarajah <harrish@codemasker.lk>
2021-01-07 22:54:22 +0530

    changes

--- js/food-item.js
+++ js/food-item.js
@@ -6,27 +6,33 @@ var fooditem = [
         name: 'Cheese Pasta',
         price: 650,
         foodPicture: '',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipi scing elit. Maecenas blandit neque ut eros cons ectetur, vel porta sem sodales. Maecenas mollis feugiat maximus. Donec vehicula tincidu nt he ndrerit. Proin pellentesque lectus ac ex ia culis, nec cursus tortor accumsan. Sed ante dui, dignissim vitae laoreet in, cursus id magna.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipi scing elit. Maecenas blandit neque ut eros cons ectetur, vel porta sem sodales. Maecenas mollis feugiat maximus. Donec vehicula tincidu nt he ndrerit. Proin pellentesque lectus ac ex ia culis, nec cursus tortor accumsan. Sed ante dui, dignissim vitae laoreet in, cursus id magna.',
+        quantity: 1
     },
     {
         id: 'm2',
         name: 'Chico Fruit Salad',
         price: 650,
         foodPicture: '',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipi scing elit. Maecenas blandit neque ut eros cons ectetur, vel porta sem sodales. Maecenas mollis feugiat maximus. Donec vehicula tincidu nt he ndrerit. Proin pellentesque lectus ac ex ia culis, nec cursus tortor accumsan. Sed ante dui, dignissim vitae laoreet in, cursus id magna.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipi scing elit. Maecenas blandit neque ut eros cons ectetur, vel porta sem sodales. Maecenas mollis feugiat maximus. Donec vehicula tincidu nt he ndrerit. Proin pellentesque lectus ac ex ia culis, nec cursus tortor accumsan. Sed ante dui, dignissim vitae laoreet in, cursus id magna.',
+        quantity: 1
     },
     {
         id: 'm3',
         name: 'Squash Risotto',
         price: 650,
         foodPicture: '',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipi scing elit. Maecenas blandit neque ut eros cons ectetur, vel porta sem sodales. Maecenas mollis feugiat maximus. Donec vehicula tincidu nt he ndrerit. Proin pellentesque lectus ac ex ia culis, nec cursus tortor accumsan. Sed ante dui, dignissim vitae laoreet in, cursus id magna.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipi scing elit. Maecenas blandit neque ut eros cons ectetur, vel porta sem sodales. Maecenas mollis feugiat maximus. Donec vehicula tincidu nt he ndrerit. Proin pellentesque lectus ac ex ia culis, nec cursus tortor accumsan. Sed ante dui, dignissim vitae laoreet in, cursus id magna.',
+        quantity: 1
     }
 ];
 
 var cart = [];
+var foodQuantity = 1;
 
 $(document).ready(function () {
+    console.log('food-item');
+    $('#display-quantity').text(foodQuantity);
     $(window).on('resize', function () {
         var win = $(this);
         if (win.width() < 768) {
@@ -53,13 +59,20 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 
 function addToCart(id) {
-    cart = JSON.parse(sessionStorage.getItem('cart'));
+    // var quan = 5;
+    var x = 0;
+    // cart = JSON.parse(sessionStorage.getItem('cart'));
     fooditem.forEach(function (item) {
         if (item.id == id) {
+            item.quantity = foodQuantity;
             cart.push(item);
         }
     })
+    console.log(cart);
     sessionStorage.setItem('cart', JSON.stringify(cart));
+    console.log(sessionStorage);
+    debugger;
+    document.location.href = "./customer-vendor.html";
 }
 
 function handleFoodItemPopup(action, id) {
@@ -78,5 +91,23 @@ function handleFoodItemPopup(action, id) {
             $('body').css('overflow', 'auto');
             $("#food-item-popup").popup("close");
         }
+    }
+}
+
+function quantity(operation) {
+    // debugger;
+    // var oriQuantity = $('#display-quantity').value;
+    // var oriQuantity = document.getElementById('display-quantity').value;
+    console.log(foodQuantity)
+    var x;
+    if (operation == 'plus') {
+        console.log('plus');
+        x = foodQuantity = foodQuantity +1;
+        $('#display-quantity').text(x);
+    } else 
+    if (operation == 'minus') {
+        console.log('minus');
+        x = foodQuantity = foodQuantity -1;
+        $('#display-quantity').text(x);
     }
 }
