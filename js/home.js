d7331be1ea9bd3cc3cbbf7a4f2d6692f29c9bcf1
Aravinthan <aravinthan0015@gmail.com>
2021-01-10 20:30:08 +0530

    Home, search Changes

--- js/home.js
+++ js/home.js
@@ -5,3 +5,10 @@ function goToVendor(vendorID) {
     sessionStorage.setItem('vendorID', vendorID);
     document.location.href = '../components/customer-vendor.html?vendorID=' + vendorID;
 }
+
+$(document).ready(function () {
+    $(".item").click(function () {
+        $(".item.active-state").removeClass("active-state");
+        $(this).addClass("active-state");
+    });
+});
