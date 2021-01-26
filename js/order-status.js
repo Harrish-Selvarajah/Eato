4caa50c0de34faaf77bd42cb52c4faf9f35b4c6b
nasifnuh <nasifnuh@gmail.com>
2021-01-27 00:41:40 +0530

    Custom marker, route color

--- js/order-status.js
+++ js/order-status.js
@@ -8,19 +8,47 @@ let map;
 
 function initMap() {
     const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer();
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 6.904333, lng: 79.867488 },
         zoom: 15,
         mapTypeId: "roadmap",
         disableDefaultUI: true,
     });
-    directionsRenderer.setMap(map);
+    var directionsRenderer = new google.maps.DirectionsRenderer({
+        map: map,
+        suppressMarkers: true,
+        polylineOptions: {
+            strokeColor: "#FD6921"
+        }
+    });
 
     calculateAndDisplayRoute(directionsService, directionsRenderer);
 }
 
-async function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    // var start_icon_path = `file:///../assets/start_location.png`
+    var icons = {
+        start: new google.maps.MarkerImage(
+            // URL
+            'https://firebasestorage.googleapis.com/v0/b/eato-69.appspot.com/o/location_32.png?alt=media&token=e716fdc4-44ae-49d8-aeb2-737a838c3524',
+            // (width,height)
+            new google.maps.Size(48, 48),
+            // The origin point (x,y)
+            new google.maps.Point(0, 0),
+            // The anchor point (x,y)
+            new google.maps.Point(15, 0)
+        ),
+        end: new google.maps.MarkerImage(
+            // URL
+            'https://firebasestorage.googleapis.com/v0/b/eato-69.appspot.com/o/location_32.png?alt=media&token=e716fdc4-44ae-49d8-aeb2-737a838c3524',
+            // (width,height)
+            new google.maps.Size(48, 48),
+            // The origin point (x,y)
+            new google.maps.Point(0, 0),
+            // The anchor point (x,y)
+            new google.maps.Point(22, 32)
+        )
+    };
     directionsService.route(
         {
             origin: {
@@ -34,9 +62,21 @@ async function calculateAndDisplayRoute(directionsService, directionsRenderer) {
         (response, status) => {
             if (status === "OK") {
                 directionsRenderer.setDirections(response);
+                var leg = response.routes[0].legs[0];
+                makeMarker(leg.start_location, icons.start, "title");
+                makeMarker(leg.end_location, icons.end, 'title');
             } else {
                 window.alert("Directions request failed due to " + status);
             }
         }
     );
+}
+
+function makeMarker(position, icon, title) {
+    new google.maps.Marker({
+        position: position,
+        map: map,
+        icon: icon,
+        title: title
+    });
 }
